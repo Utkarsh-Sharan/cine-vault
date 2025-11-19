bfb976a2e2869559442da33f18144ae23c1675e0
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-19 11:46:03 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolore deleniti dignissimos eos natus minus veniam nihil vero at nesciunt dolor voluptatibus impedit vitae velit amet, molestias et totam error.</h1>
+    </>
+  )
+}
+
+export default App

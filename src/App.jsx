999354d52876b15ee1b0e6fb5d47d590786ad78f
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-20 12:07:10 +0530

    set hero image

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-black underline">Hi</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="main-image" />
+
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
   );
 };
 

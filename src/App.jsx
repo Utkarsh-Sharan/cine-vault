e8021dd41c9ba5ad384f022e7a7f25748e4e120c
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-11-20 11:06:49 +0530

    revise basic terminologies of react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-function App() {
+//This is a component.
+function Card({ title }) {
+  //hasLiked is a State
+  //useState is a react hook
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  });
+
   return (
     <>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolore deleniti dignissimos eos natus minus veniam nihil vero at nesciunt dolor voluptatibus impedit vitae velit amet, molestias et totam error.</h1>
+      <div className="card rounded-md bg-orange-400 flex-col justify-center gap-4">
+        <h2>{title}</h2>
+        <button
+          onClick={() => {
+            setHasLiked((prevState) => (prevState = !prevState));
+          }}
+          className="py-2 px-5 bg-violet-500 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75"
+        >
+          {hasLiked ? "Liked" : "Like"}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  /*the title is called as a "prop".*/
+  return (
+    <>
+      <h1 className="bg-red-200">The Movies:</h1>
+      <Card title="Mahavatar Narsimha" />
+      <Card title="Kurukshetra" />
+      <Card title="Ramayan: Part-1" />
+    </>
+  );
+}
+
+export default App;
